359731c4f707c02bf24da9af2255642b5f8c49ff
devofficial <debasisnath85@gmail.com>
2018-09-06 17:50:54 +0530

    setup the server

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-import { express } from "express";
+import express from "express";
+
+
+const app =  express();
+const PORT = 3000;
+
+
+
+// connecting to the server
+app.listen(PORT, () => console.log(`the app is listening on port ${PORT}`));
